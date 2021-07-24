5c59c9f9d645729fc52bee28c430e49a511bc9e2
cyberblack28 <y.ichikawa2014@gmail.com>
2021-07-24 15:57:39 +0900

    test

--- app/main.go
+++ app/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello GitOps CICD Conference!!")
+	fmt.Fprintf(w, "Hello GitOps CICD Conference new!!")
 }
 
 func main() {
